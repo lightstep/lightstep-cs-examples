7e50fd400115b75890b4444f7b219f920c05d902
jmacd <jmacd@lightstep.com>
2020-10-06 16:19:15 -0700

    Add a summary

--- Metrics/promlocal/cmd/main.go
+++ Metrics/promlocal/cmd/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"log"
+	"math/rand"
 	"net/http"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -20,14 +22,51 @@ var (
 		},
 		[]string{"device"},
 	)
+	responseDuration = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name: "response_duration",
+			Help: "Response time in seconds.",
+		},
+		[]string{"client_id"},
+	)
+	userAge = prometheus.NewSummaryVec(
+		prometheus.SummaryOpts{
+			Name:       "age",
+			Help:       "Age in years.",
+			Objectives: map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.99: 0.001},
+		},
+		[]string{"city"},
+	)
 )
 
 func twoMetrics(registry prometheus.Registerer) {
 	registry.MustRegister(cpuTemp)
 	registry.MustRegister(hdFailures)
+	registry.MustRegister(responseDuration)
+	registry.MustRegister(userAge)
 
 	cpuTemp.Set(65.3)
 	hdFailures.With(prometheus.Labels{"device": "/dev/sda"}).Inc()
+	h0 := responseDuration.With(prometheus.Labels{"client_id": "000"})
+	h1 := responseDuration.With(prometheus.Labels{"client_id": "001"})
+	h2 := responseDuration.With(prometheus.Labels{"client_id": "002"})
+	sSF := userAge.With(prometheus.Labels{"city": "SF"})
+	sLA := userAge.With(prometheus.Labels{"city": "LA"})
+
+	go func() {
+		for {
+			time.Sleep(time.Second)
+			for i := 0; i < 5; i++ {
+				h0.Observe(0.001)
+				h1.Observe(0.1)
+				h2.Observe(10)
+			}
+			for i := 0; i < 100; i++ {
+				sSF.Observe(rand.ExpFloat64())
+				sLA.Observe(rand.NormFloat64())
+			}
+		}
+	}()
 }
 
 func main() {
