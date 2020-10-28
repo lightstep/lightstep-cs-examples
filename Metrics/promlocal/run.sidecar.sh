7a999508704a1555feff901c5584118a9044f8bc
Josh MacDonald <jmacd@lightstep.com>
2020-10-28 13:35:08 -0700

    More config

--- Metrics/promlocal/run.sidecar.sh
+++ Metrics/promlocal/run.sidecar.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-DIR=/Users/jmacd/src/lightstep/go/src/github.com/lightstep/lightstep-prometheus-sidecar
+DIR=/Users/loaner/src/lightstep/go/src/github.com/lightstep/opentelemetry-prometheus-sidecar
 TOKEN=7d74998428f3f0bfd7659bcc387b4e06
+
 #    --security.server-certificate=certs/Server.crt \
 #    --opentelemetry.api-address=https://127.0.0.1:7000 \
 
 #    --opentelemetry.api-address=https://ingest.staging.lightstep.com:443/metrics/otlp/v0.5 \
 
-((cd ${DIR} && go build ./cmd/lightstep-prometheus-sidecar) && \
-   ${DIR}/lightstep-prometheus-sidecar \
-    --security.server-certificate=certs/Server.crt \
-    --opentelemetry.api-address=https://127.0.0.1:7000 \
+((cd ${DIR} && go build ./cmd/opentelemetry-prometheus-sidecar) && \
+   ${DIR}/opentelemetry-prometheus-sidecar \
+    --opentelemetry.api-address=https://ingest.staging.lightstep.com:443/metrics/otlp/v0.5 \
     --prometheus.wal-directory=cfg/data/wal \
     --grpc.header="Lightstep-Access-Token=${TOKEN}" \
     --resource.attribute="Checkers=Red" \
