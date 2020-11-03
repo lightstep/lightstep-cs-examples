bf02b7857399825e79709a5a1ff32ddc6026594e
Josh MacDonald <jmacd@lightstep.com>
2020-11-02 22:40:17 -0800

    Checkpoint

--- Metrics/promlocal/go.mod
+++ Metrics/promlocal/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gogo/protobuf v1.3.1
 	github.com/golang/protobuf v1.4.2
 	github.com/prometheus/client_golang v1.7.1
+	golang.org/x/tools/gopls v0.5.2 // indirect
 	google.golang.org/genproto v0.0.0-20200929141702-51c3e5b607fe
 	google.golang.org/grpc v1.27.0
 )
--- Metrics/promlocal/run.sidecar.sh
+++ Metrics/promlocal/run.sidecar.sh
@@ -1,18 +1,36 @@
 #!/bin/bash
 
 DIR=/Users/loaner/src/lightstep/go/src/github.com/lightstep/opentelemetry-prometheus-sidecar
-TOKEN=7d74998428f3f0bfd7659bcc387b4e06
+TOKEN=5fKQnjSA+LYfH5ODStKrSMDGgFNpsgZaKtrqJ5P9RNLQVD9UPFcOCFyE2f8SamoVNbUTKb33zcVMTI+vXVY=
 
 #    --security.server-certificate=certs/Server.crt \
-#    --opentelemetry.api-address=https://127.0.0.1:7000 \
-
-#    --opentelemetry.api-address=https://ingest.staging.lightstep.com:443/metrics/otlp/v0.5 \
+#    --opentelemetry.api-address=ingest.staging.lightstep.com:443
+#    --opentelemetry.api-address=127.0.0.1:55679 \
+#    --log.level=info \
 
 ((cd ${DIR} && go build ./cmd/opentelemetry-prometheus-sidecar) && \
    ${DIR}/opentelemetry-prometheus-sidecar \
-    --opentelemetry.api-address=https://ingest.staging.lightstep.com:443/metrics/otlp/v0.5 \
     --prometheus.wal-directory=cfg/data/wal \
+    --opentelemetry.endpoint=https://ingest.staging.lightstep.com:443 \
     --grpc.header="Lightstep-Access-Token=${TOKEN}" \
-    --resource.attribute="Checkers=Red" \
-    --resource.attribute="Switch=Nintendo" \
-    --log.level=debug)
+    --resource.attribute="service.name=promdemo" \
+    --resource.attribute="board.game=checkers" \
+    --resource.attribute="video.game=switch" \
+    --log.level=info \
+    --startup.delay=1s \
+    \
+    --opentelemetry.diagnostics-endpoint=https://ingest.staging.lightstep.com:443 \
+    --grpc.diagnostics-header="Lightstep-Access-Token=${TOKEN}" \
+    --grpc.diagnostics-header="Lightstep-Reporting-Agent=opentelemetry-prometheus-sidecar" \
+    --resource.diagnostics-attribute="service.name=promdiag" \
+    --resource.diagnostics-attribute="board.game=scrabble" \
+    --resource.diagnostics-attribute="video.game=playstation" \
+   )
+
+## TODO: Check it works w/o :443
+
+#    --opentelemetry.diagnostics-endpoint=ingest.staging.lightstep.com:443 \
+#    --grpc.diagnostics-header="Lightstep-Access-Token=${TOKEN}" \
+#    --grpc.diagnostics-header="Lightstep-Reporting-Agent=opentelemetry-prometheus-sidecar" \
+#    --resource.diagnostics-attribute="Checkers=Black" \
+#    --resource.diagnostics-attribute="Playstation=Sony" \
