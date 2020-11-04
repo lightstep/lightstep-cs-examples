8d04f6362015dddbb337455479c18bbfb95bf9fd
Josh MacDonald <jmacd@lightstep.com>
2020-11-04 01:40:45 -0800

    Add config file

--- Metrics/promlocal/run.sidecar.sh
+++ Metrics/promlocal/run.sidecar.sh
@@ -8,23 +8,26 @@ TOKEN=5fKQnjSA+LYfH5ODStKrSMDGgFNpsgZaKtrqJ5P9RNLQVD9UPFcOCFyE2f8SamoVNbUTKb33zc
 #    --opentelemetry.api-address=127.0.0.1:55679 \
 #    --log.level=info \
 
+    # --prometheus.wal-directory=cfg/data/wal \
+    # --opentelemetry.endpoint=https://ingest.staging.lightstep.com:443 \
+    # --grpc.header="Lightstep-Access-Token=${TOKEN}" \
+    # --resource.attribute="service.name=promdemo" \
+    # --resource.attribute="board.game=checkers" \
+    # --resource.attribute="video.game=switch" \
+    # --log.level=info \
+    # --startup.delay=1s \
+    # \
+    # --opentelemetry.diagnostics-endpoint=https://ingest.staging.lightstep.com:443 \
+    # --grpc.diagnostics-header="Lightstep-Access-Token=${TOKEN}" \
+    # --grpc.diagnostics-header="Lightstep-Reporting-Agent=opentelemetry-prometheus-sidecar" \
+    # --resource.diagnostics-attribute="service.name=promdiag" \
+    # --resource.diagnostics-attribute="board.game=scrabble" \
+    # --resource.diagnostics-attribute="video.game=playstation" \
+
 ((cd ${DIR} && go build ./cmd/opentelemetry-prometheus-sidecar) && \
-   ${DIR}/opentelemetry-prometheus-sidecar \
-    --prometheus.wal-directory=cfg/data/wal \
-    --opentelemetry.endpoint=https://ingest.staging.lightstep.com:443 \
-    --grpc.header="Lightstep-Access-Token=${TOKEN}" \
-    --resource.attribute="service.name=promdemo" \
-    --resource.attribute="board.game=checkers" \
-    --resource.attribute="video.game=switch" \
-    --log.level=info \
-    --startup.delay=1s \
-    \
-    --opentelemetry.diagnostics-endpoint=https://ingest.staging.lightstep.com:443 \
-    --grpc.diagnostics-header="Lightstep-Access-Token=${TOKEN}" \
-    --grpc.diagnostics-header="Lightstep-Reporting-Agent=opentelemetry-prometheus-sidecar" \
-    --resource.diagnostics-attribute="service.name=promdiag" \
-    --resource.diagnostics-attribute="board.game=scrabble" \
-    --resource.diagnostics-attribute="video.game=playstation" \
+     ${DIR}/opentelemetry-prometheus-sidecar \
+	   --grpc.header="Lightstep-Access-Token=${TOKEN}" \
+	   --config-file="sidecar.yaml" \
    )
 
 ## TODO: Check it works w/o :443
