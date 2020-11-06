6afa17065085e57b291e142e6e04f27da15497ab
Ishmeet Grewal <ishmeet@lightstep.com>
2020-11-05 16:37:24 -0800

    Addin Creative Tim UI Template

---
+++ api-cookbook/trace-differ/client/src/components/BaseSwitch.vue
@@ -0,0 +1,30 @@
+<template>
+  <label class="custom-toggle">
+    <input v-model="model" type="checkbox" v-bind="$attrs" v-on="$listeners" />
+    <span class="custom-toggle-slider rounded-circle"></span>
+  </label>
+</template>
+<script>
+export default {
+  name: 'BaseSwitch',
+  inheritAttrs: false,
+  props: {
+    value: {
+      type: Boolean,
+      default: false,
+      description: 'Switch value'
+    }
+  },
+  computed: {
+    model: {
+      get() {
+        return this.value
+      },
+      set(value) {
+        this.$emit('input', value)
+      }
+    }
+  }
+}
+</script>
+<style></style>
