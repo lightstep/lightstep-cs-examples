b872975c2107b86f5275e2bf3c99a4e5102dd6ee
Ishmeet Grewal <ishmeetsinghgrewal@gmail.com>
2021-03-23 17:45:30 -0700

    WIP 70%

---
+++ api-cookbook/auto-streams/README.md
@@ -0,0 +1,7 @@
+TODO
+
+- Error streams
+- Dashboards with the stream (if more than one stream ends up being created)
+- Configurable for "types of streams"
+- Dockerized
+- K8s Cron Job
--- api-cookbook/auto-streams/src/constants.js
+++ api-cookbook/auto-streams/src/constants.js
@@ -1,7 +1,7 @@
 module.exports = {
   LIGHTSTEP_HOST: 'https://api.lightstep.com',
   LIGHTSTEP_ORG: process.env.LIGHTSTEP_ORG || 'LightStep',
-  LIGHTSTEP_PROJECT: process.env.LIGHTSTEP_PROJECT || 'dev-ishmeet',
+  LIGHTSTEP_PROJECT: process.env.LIGHTSTEP_PROJECT || 'demo',
   LIGHTSTEP_API_KEY: process.env.LIGHTSTEP_API_KEY || '',
   LIGHTSTEP_ACCESS_TOKEN: process.env.LIGHTSTEP_ACCESS_TOKEN || '',
   SCHEDULE_RULE_MINUTES: 1
